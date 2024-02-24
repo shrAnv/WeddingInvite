cf4d9a0ca4eb066f7e081104cfbd7af1fef1d9dc
shrAnv <158760418+shrAnv@users.noreply.github.com>
2024-02-24 23:25:48 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,3 +1,14 @@
-document.getElementById('rsvpBtn').addEventListener('click', function() {
-    alert('Thank you for RSVPing! We look forward to celebrating with you.');
+document.addEventListener("DOMContentLoaded", function() {
+    const elements = document.querySelectorAll(".element");
+
+    function showElements() {
+        elements.forEach((element, index) => {
+            setTimeout(() => {
+                element.classList.add("show");
+            }, index * 60000); // Adjust the delay (in milliseconds) between elements
+        });
+    }
+
+    // Call the function to show elements when the page is loaded
+    showElements();
 });
