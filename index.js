6424c623051f62eb8c3ca1d1b9c7b14146e15ee5
shrAnv <158760418+shrAnv@users.noreply.github.com>
2024-02-25 00:43:22 +0530

    Update index.js
    updated js for adjusting size

--- index.js
+++ index.js
@@ -1,14 +1,29 @@
-document.addEventListener("DOMContentLoaded", function() {
-    const elements = document.querySelectorAll(".element");
+        document.addEventListener('DOMContentLoaded', function () {
+            const elements = document.querySelectorAll(".element");
 
-    function showElements() {
-        elements.forEach((element, index) => {
-            setTimeout(() => {
-                element.classList.add("show");
-            }, index * 60000); // Adjust the delay (in milliseconds) between elements
+            function showElements() {
+                elements.forEach((element, index) => {
+                    setTimeout(() => {
+                        element.classList.add("show");
+                    }, index * 60000); // Adjust the delay (in milliseconds) between elements
+                });
+            }
+
+            // Call the function to show elements when the page is loaded
+            showElements();
+
+            var backgroundImage = new Image();
+            backgroundImage.src = "C:/Users/DELL/OneDrive/Documents/weddinginvitre/images/download (1).jpeg";
+
+            backgroundImage.onload = function () {
+                var element = document.getElementById('dynamicElement');
+                var imageSize = {
+                    width: backgroundImage.width,
+                    height: backgroundImage.height
+                };
+
+                // Set the size of the element based on the image size
+                element.style.width = imageSize.width + 'px';
+                element.style.height = imageSize.height + 'px';
+            };
         });
-    }
-
-    // Call the function to show elements when the page is loaded
-    showElements();
-});
